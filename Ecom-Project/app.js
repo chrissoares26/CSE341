ced32e539fbfa15cecc75d968be4bb8646e8143b
chrissoares26 <sou20001@byui.edu>
2021-09-23 15:55:54 -0600

    prove 2

--- Ecom-Project/app.js
+++ Ecom-Project/app.js
@@ -1,17 +1,19 @@
-const http = require('http');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const express =  require('express');
-const path = require('path');
+
+
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData =  require('./routes/admin')
 const shopRoutes =  require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
@@ -19,8 +21,8 @@ app.use('/admin', adminData.routes);
 
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
-});
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  });
 
 
 app.listen(3000);
--- Ecom-Project/routes/admin.js
+++ Ecom-Project/routes/admin.js
@@ -9,11 +9,11 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product',(req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product'})
+    res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product', activeAddProduct: true, formsCSS: true, productCSS: true})
 });
 
 router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title });
+    products.push({ title: req.body.title, price: req.body.price, description: req.body.description});
     res.redirect('/');
 });
 
